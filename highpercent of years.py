6336cfb6dc5c7e5864cfe5ab5eb7fdf329f809ff
Chaninpp <43023707+Chaninpp@users.noreply.github.com>
2018-12-13 20:03:23 +0700

    Add files via upload

---
+++ highpercent of years.py
@@ -0,0 +1,107 @@
+"""PSIT"""
+def highpercent():
+    """this function to use find a high percent to birth in hospital"""
+    import csv
+    with open('Year 2551.txt') as csvfile:
+        reader = csv.reader(csvfile)
+        lst = []
+        dic = {}
+        for i in reader:
+            lst == lst.append(i[-1])
+            dic[i[-1]] = i[1]
+        lst.sort()
+        print("Year 2551", dic[lst[-2]], lst[-2])
+
+
+    with open('Year 2552.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2552', dic[lst[-2]], lst[-2])
+
+
+    with open('Year 2553.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2553', dic[lst[-2]], lst[-2])
+
+
+    with open('Year 2554.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2554', dic[lst[-2]], lst[-2])
+
+    with open('Year 2555.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2555', dic[lst[-2]], lst[-2])
+
+    with open('Year 2556.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2556', dic[lst[-2]], lst[-2])
+
+    with open('Year 2557.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2557', dic[lst[-2]], lst[-2])
+
+    with open('Year 2558.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2558', dic[lst[-2]], lst[-2])
+
+    with open('Year 2559.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2559', dic[lst[-2]], lst[-2])
+
+    with open('Year 2560.txt') as csvfile:
+            reader = csv.reader(csvfile)
+            lst = []
+            dic = {}
+            for i in reader:
+                lst == lst.append(i[-1])
+                dic[i[-1]] = i[1]
+            lst.sort()
+            print('Year 2560', dic[lst[-2]], lst[-2])
+highpercent()
